5644f0f87a5ea9ec14d0b76344aaf2c48b60267a
priyal-gopawat <priyalgopawat11111@gmail.com>
2021-05-20 12:42:55 +0530

    6.2 Completed

---
+++ app/src/main/java/com/example/myecom/Constants.java
@@ -0,0 +1,5 @@
+package com.example.myecom;
+
+public class Constants {
+    public static final String VALUE = "value" ;
+}
--- app/src/main/java/com/example/myecom/MainActivity.java
+++ app/src/main/java/com/example/myecom/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.myecom;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
@@ -22,6 +25,33 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(b.getRoot());
 
+        //receiving data from saved instance
+        if(savedInstanceState!=null) {
+            qty = savedInstanceState.getInt(Constants.VALUE, 0);
+        }
+            else{
+                SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+                qty = prefs.getInt(Constants.VALUE,0);
+            }
+        b.qty.setText("" + qty);
+    }
+
+    // Saving data by saved instance
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(Constants.VALUE,qty);
+    }
+
+    // Saved data by shared preferences
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+        prefs.edit()
+                .putInt(Constants.VALUE,qty)
+                .apply();
     }
 
     /**
